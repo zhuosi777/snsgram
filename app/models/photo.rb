eb3d093cb4030597b681b41f78d449dbe7663aad
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-08 18:24:13 +0900

    gem追加ルート記述変更

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :post
 
   validates :iamge, presence: true
+
+  mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     controllers: { registrations: "registrations" }
   root 'pages#home'
   get "/users/:id", to: "users#show", as:"user"
+  
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
 end
