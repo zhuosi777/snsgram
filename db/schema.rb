52f4f941a9d2f421f978dd814fe237cda22151e6
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-08 18:14:05 +0900

    post修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_085459) do
+ActiveRecord::Schema.define(version: 2019_12_08_085838) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_photos_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
