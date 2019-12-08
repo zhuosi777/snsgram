4470d95e55fc5450bfb7d18ad74700df41f3c938
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-08 20:19:58 +0900

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
       resources :likes, only: %i(create destroy)
+        resources :comments, only: %i(create destroy)
   end
 end
