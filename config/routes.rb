d660d89d50d6c2c19913de4561616ca5822b8491
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-08 17:53:17 +0900

    editページ編集完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
     controllers: { registrations: "registrations" }
   root 'pages#home'
   get "/users/:id", to: "users#show", as:"user"
